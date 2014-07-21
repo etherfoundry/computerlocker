80296cbf18b53dbda0404414c2dbcf66135a48a0
etherfoundry <etherfoundry@void-media.net>
2014-07-21 13:06:56 -0400

    Initial Commit

---
+++ ComputerLocker/ComputerLocker.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ComputerLocker
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrCheckLock = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tmrCheckLock
+            // 
+            this.tmrCheckLock.Enabled = true;
+            this.tmrCheckLock.Interval = 1000;
+            this.tmrCheckLock.Tick += new System.EventHandler(this.tmrCheckLock_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(225, 0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Computer Locker";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrCheckLock;
+    }
+}
+
---
+++ ComputerLocker/ComputerLocker.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace ComputerLocker
+{
+    
+
+    public partial class frmMain : Form
+    {
+        [DllImport("user32")]
+        public static extern void LockWorkStation();
+
+        public bool SessionLocked = false;
+
+        public frmMain()
+        {
+            Microsoft.Win32.SystemEvents.SessionSwitch += new Microsoft.Win32.SessionSwitchEventHandler(SystemEvents_SessionSwitch);
+            InitializeComponent();
+        }
+
+        private bool checkLockCondition()
+        {
+            bool result = false;
+            if (this.SessionLocked)
+            {
+                return false;
+            }
+
+            if (DateTime.Now.Hour > 1 && DateTime.Now.Hour < 6 && DateTime.Now.DayOfWeek != DayOfWeek.Saturday)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+        
+        private void lockPc()
+        {
+            LockWorkStation();
+        }
+
+        private void tmrCheckLock_Tick(object sender, EventArgs e)
+        {
+            if (checkLockCondition())
+            {
+                lockPc();
+            }
+        }
+
+        private void SystemEvents_SessionSwitch(object sender, Microsoft.Win32.SessionSwitchEventArgs e)
+        {
+            if(e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                this.SessionLocked = false;
+                if(checkLockCondition())
+                {
+                    lockPc();
+                }
+            }
+            if(e.Reason == SessionSwitchReason.SessionLock)
+            {
+                this.SessionLocked = true;
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+}
